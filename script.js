f77abab463e82468d890171af50cbc76704ca7bb
Victoria_T <Tabordaingridvictoria@outlook.es>
2021-10-19 23:41:21 -0300

    CambiEnElJs

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function click(){
-    var contenedor = document.querySelector(".contenedor")
+//function click(){
+  //  let contenedor = document.querySelector(".contenedor")
+   // console.log(contenedor);
+  //  let enlaceCrear = document.getElementById("crearCuenta")
+   // console.log(enlaceCrear);
+  // enlaceCrear.onclick= function(){
+  //      contenedor.classList.toggle("active")
+  //  }
+//}
+
+let contenedor = document.querySelector(".contenedor")
+let enlaceCrear = document.getElementById("crearCuenta")
+enlaceCrear.onclick = function(){    
     contenedor.classList.toggle("active")
+    
 }
 
+
+
+let contenedor2 = document.querySelector(".contenedor")
+let enlaceCrear2 = document.getElementById("crearCuenta2")
+enlaceCrear2.onclick = function(){    
+    contenedor.classList.toggle("active")
+    
+}
